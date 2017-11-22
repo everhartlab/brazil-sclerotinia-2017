6f093be8d789493e2f97221197feeb3479836885
Zhian Kamvar <zkamvar@gmail.com>
2017-11-22 16:08:07 -0600

    update installation

--- packages.R
+++ packages.R
@@ -4,8 +4,8 @@ get_package <- function(x){
             if (!require("devtools")){
                 install.packages("devtools", repos = "https://cran.rstudio.com")
                 require("devtools")
-                install_github("zkamvar/ggcompoplot")
             }
+            install_github("zkamvar/ggcompoplot")
         }
         install.packages(x, repos = "https://cran.rstudio.com")
     }
