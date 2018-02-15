9a5f1f02c9da8054240d35576ad3dd2afaa6fe86
Zhian Kamvar <zkamvar@gmail.com>
2018-02-15 13:06:58 -0600

    modify MSN to use pre-saved data

--- MSN.R
+++ MSN.R
@@ -1,32 +1,18 @@
-library(poppr)
+library("poppr")
 library("igraph")
 enc <- getOption("encoding")
 in_script <- !interactive()
 options(encoding = "iso-8859-1")
-CD <- read.genalex(here::here("data", "data.csv")) #"~/Thesis Project/Data Analysis/Raw Data/Compiled Data AN 2.csv") 
-my_palette <- c("Nebraska" = "#000000",
-                "Argentina" = "#F0E442", # "#E69F00",
-                "Bahia" = "#56B4E9",
-                "Góias" = "#009E73",
-                "Mato Grosso do Sul" = "#E69F00",
-                "Minas Gerias" = "#0072B2",
-                "Paraná" = "#D55E00",
-                "Rio Grande do Sul" = "#CC79A7")
-splitStrata(CD) <- ~Continent/Country/Population
-setPop(CD) <- ~Population
-CDrepet <- c(2,6,2,2,2,2,4,4,4,4,3)
+CD <- readRDS(here::here("data", "full-genclone-object.rds"))
 
-CD_sub <- popsub(CD, blacklist = character(0))
-# ZNK: add and loss are parameters specifically for polyploid data with missing 
-#      data and are both TRUE by default. You do not need to specify them here.
-min_span_net <- bruvo.msn(CD_sub, replen = c(CDrepet), add = TRUE, loss = TRUE, showplot = FALSE, include.ties = TRUE) 
+min_span_net <- bruvo.msn(CD, replen = other(CD)$REPLEN,  showplot = FALSE, include.ties = TRUE) 
 set.seed(69)
 min_span_net <- plot_poppr_msn(CD,
                min_span_net,
                inds = "NONE",
                mlg = FALSE,
                gadj = 3,
-               palette = my_palette,
+               palette = other(CD)$palette,
                cutoff = NULL,
                quantiles = FALSE,
                beforecut = TRUE,
@@ -72,7 +58,7 @@ plot.igraph(min_span_net$graph,
             layout = lay)
 
 # Create population legend and save it into variable "a"
-sortpop <- names(my_palette)
+sortpop <- names(other(CD)$palette)
 a <- legend("topleft", 
        legend = sortpop, 
        fill = min_span_net$colors[sortpop])
