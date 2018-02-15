f239bc286331be7db93599e059dcc3782cb4920b
Zhian Kamvar <zkamvar@gmail.com>
2018-02-15 12:54:16 -0600

    use saved data and add counts to MCG fig

--- GeneralPopDetails.R
+++ GeneralPopDetails.R
@@ -11,73 +11,22 @@
 # Setup -------------------------------------------------------------------
 #
 #
-# setwd("~/Thesis Project/Data Analysis")
 library(poppr)
 library(tidyverse)
 if (!interactive()) options(width = 200)
 enc <- getOption("encoding")
 options(encoding = "iso-8859-1")
-CD <- read.genalex(here::here("data", "data.csv")) #"~/Thesis Project/Data Analysis/Raw Data/Compiled Data AN 2.csv")
-splitStrata(CD) <- ~Continent/Country/Population
-setPop(CD) <- ~Population
-my_palette <- c("Nebraska" = "#000000",
-                "Argentina" = "#F0E442", # "#E69F00",
-                "Bahia" = "#56B4E9",
-                "Góias" = "#009E73",
-                "Mato Grosso do Sul" = "#E69F00",
-                "Minas Gerias" = "#0072B2",
-                "Paraná" = "#D55E00",
-                "Rio Grande do Sul" = "#CC79A7")
+CD <- readRDS(here::here("data", "full-genclone-object.rds"))
 
 
-# MCG determination -------------------------------------------------------
-#
-# This section is for adding the MCG data collected by T. Miorini. One thing to
-# note about these data is that there are only a subset of these samples that
-# were phenotyped. We are reporting these data for posterity.
-# 
-# This table was copied from a table produced by T. Miorini
-MCG_table <- data.frame(stringsAsFactors = FALSE,
-                        MCG = c("A", "A", "A", "B", "B", "C", "D", "E", "F", "G", "H", "I",
-                                "I", "I", "J", "J", "K", "K", "K", "L", "L", "M", "N", "N",
-                                "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N",
-                                "N", "O", "O", "O", "O", "O", "O", "P", "P", "P", "Q", "Q", "Q",
-                                "Q", "Q", "R", "R", "R", "R", "R", "S", "S", "T", "T", "T", "T",
-                                "U", "U", "V", "W", "Y", "Z"),
-                        Isolate = c("143", "276", "266", "399A", "399B", "293A", "293B", "293C",
-                                    "293D", "293E", "265", "267", "977C", "977D", "268", "541B",
-                                    "202", "264", "289", "834A", "834B", "541A", "973A", "977E",
-                                    "978A", "978B", "978C", "978D", "978E", "979B", "979D", "979E",
-                                    "1015", "1016", "1017", "1019", "1022", "1023", "974A", "974B",
-                                    "974D", "974E", "979A", "979C", "973B", "973C", "973D", "975A",
-                                    "975E", "1012", "1013", "1014", "975B", "975C", "1010", "1011",
-                                    "1018", "976A", "976C", "976B", "977A", "976D", "976E", "977B",
-                                    "973E", "974C", "975D", "1020", "1021"),
-                        Year = c(1977L, 1996L, 1994L, 1999L, 1999L, 1996L, 1996L, 1996L, 1996L,
-                                 1996L, 1994L, 1994L, 2012L, 2012L, 1994L, 2004L, 1990L, 1994L,
-                                 1996L, 2009L, 2009L, 2004L, 2012L, 2012L, 2012L, 2012L, 2012L,
-                                 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L,
-                                 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L,
-                                 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L,
-                                 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L, 2012L,
-                                 2012L, 2012L, 2012L, 2012L, 2012L, 2012L)
-)
+# MCG Tabulation ----------------------------------------------------------
 
-# Here, we are adding isolate names to the strata and then joining this new
-# table in the same order as the original data. Since this is but a subset of
-# the data, we are replacing all missing MCG assignments with "?"
-addStrata(CD) <- data.frame(Isolate = indNames(CD))
-newstrata <- left_join(strata(CD), MCG_table) %>%
-  mutate(MCG = case_when(is.na(MCG) ~ "?", TRUE ~ MCG))
-
-# A check to make sure we aren't shuffling names.
-stopifnot(identical(newstrata$Isolate, indNames(CD)))
-
-strata(CD) <- newstrata
-setPop(CD) <- ~Population
 
 # We can tabulate the MCGs by using them as custom multilocus genotypes.
-mll.custom(CD) <- strata(CD)$MCG
+mll.custom(CD) <- strata(CD) %>% 
+  mutate(MCG = case_when(is.na(MCG) ~ "?", 
+                         TRUE     ~ as.character(MCG))) %>% 
+  pull(MCG)
 mlg.table(CD, color = TRUE)
 
 # The plot produced needs to be cleaned up a bit:
@@ -87,12 +36,15 @@ mlg.table(CD, color = TRUE)
 #  3. We want to provide an indicator for the uncategorized MCGs
 #  4. The text size and family needs to be set. 
 #  
-p <- last_plot()
+p          <- last_plot()
+pal        <- other(CD)$palette
+mcg_counts <- colSums(table(strata(CD, ~MCG/Population, combine = FALSE)))
+names(pal) <- paste0(names(pal), " (", mcg_counts[names(pal)], ")")
 pdf(here::here("figs/MCG-bar.pdf"), width = 7.20472 * 0.6, height = 7.20472 * 0.4, pointsize = 5, colormodel = "cmyk")
 dev.control("enable") # allows me to copy to tiff device
 p %+%
-  mutate(p$data, Population = fct_relevel(Population, names(my_palette))) +
-  scale_fill_manual(values = my_palette) + 
+  mutate(p$data, Population = fct_relevel(Population, names(other(CD)$palette))) +
+  scale_fill_manual(values = other(CD)$palette, labels = names(pal)) + 
   aes(alpha = ifelse(MLG == "?", "unknown", "known"), color = I("black")) +
   scale_alpha_manual(values = c(unknown = 0.5, known = 1), guide = "none") +
   theme_bw(base_size = 10, base_family = "Helvetica") +
@@ -169,7 +121,7 @@ print(priv_fraction <- sweep(pal, 2, nAll(CD)[colnames(pal)], FUN = "/"))
 # creating table 1 --------------------------------------------------------
 # First, we can create a table of populations
 poptable <- strata(CD) %>% 
-  select(-Isolate, -MCG, -Year) %>% 
+  select(Continent, Country, Population) %>% 
   dplyr::distinct() %>% 
   select(Country, Population)
 print(poptable)
