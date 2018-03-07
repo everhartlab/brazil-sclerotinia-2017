2d07c0221aa3f47fd50492bf935daa0a7bd008bd
Zhian N. Kamvar <zkamvar@gmail.com>
2018-03-07 13:56:35 -0600

    Update install.R

--- install.R
+++ install.R
@@ -4,7 +4,7 @@ packages <- readLines("00-install.R")
 packages <- packages[grep("get_package[(]", packages)]
 packages <- gsub("get_package", "install.packages", packages)
 for (pkg in seq(packages)) {
-  if (grepl("ggcompoplot", pkg)){
+  if (grepl("ggcompoplot", packages[pkg])){
     remotes::install_github("zkamvar/ggcompoplot")
   } else {
     eval(parse(text = packages[pkg]))
